6944ed4c1cbd52425ebf110369cb6feff50d8972
steven gonsalvez <steven.gonsalvez@gmail.com>
2021-04-24 22:00:36 +0100

    steve: bots

--- terraform/user/main.tf
+++ terraform/user/main.tf
@@ -1,6 +1,8 @@
 locals {
   organization = "action-foobar"
-  userlist     = csvdecode(file("${path.module}/../../people.csv"))
+  userlist     = concat(csvdecode(file("${path.module}/../../bots.csv")),csvdecode(file("${path.module}/../../people.csv")))
+  #userlist     = csvdecode(file("${path.module}/../../bots.csv"))
+
 }
 
 provider "github" {
@@ -21,16 +23,9 @@ resource "github_membership" "this" {
   # member or admin
 }
 
-data "github_membership" "list" {
-  for_each     = { for usr in local.userlist : usr.Username => usr }
-  username     = each.value.Username
-  organization = local.organization
+output "input_user"{
+  value = local.userlist
 }
-
-output "org_membership" {
-  value = data.github_membership.list
-}
-
 output "user_details" {
   value = data.github_user.example
 }
