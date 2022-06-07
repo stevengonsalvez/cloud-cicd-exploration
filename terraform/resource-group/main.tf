7d31c189859b4c20693778ea0b376d83e89ccd7a
steven gonsalvez <steven.gonsalvez@gmail.com>
2022-06-07 12:28:22 +0100

    fix: updated pipe for oidc

---
+++ terraform/resource-group/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  # to ignore backend for these tests
+  backend "local" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.9.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  use_oidc = true
+  features {}
+}
+
+
+locals {
+  created          = time_static.activation_date.unix
+  created_readable = formatdate("YYYY-MM-DD_hh-mm-ss", time_static.activation_date.rfc3339)
+}
+
+resource "time_static" "activation_date" {}
+
+resource "azurerm_resource_group" "example" {
+  name     = "sandbox-rg-test"
+  location = "North Europe"
+  tags = {
+    ttl                    = "2h"
+    creation_time          = local.created
+    creation_time_readable = local.created_readable
+    delete_marker          = "true"
+  }
+}
